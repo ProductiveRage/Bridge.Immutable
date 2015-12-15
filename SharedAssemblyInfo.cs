1e285d52c9cb7562b174649f89a486849c798528
ProductiveRage <dangger36@gmail.com>
2015-12-15 17:47:35 +0000

    Bumped version to try VS2015RTM-compatible release

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Productive Rage 2015")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
