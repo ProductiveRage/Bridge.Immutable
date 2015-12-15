5095d9c2ce9682f9277e0fd8df274f912969c03b
ProductiveRage <dangger36@gmail.com>
2015-12-15 10:28:39 +0000

    Added Bridge as a dependency in the NuGet package

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Productive Rage 2015")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
