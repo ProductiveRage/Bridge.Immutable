dce982daba6205d462aa30cd072716c20a016046
ProductiveRage <dangger36@gmail.com>
2015-12-15 09:47:17 +0000

    Bumped version number for first release

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyCopyright("Copyright © Productive Rage 2015")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
