5c68b830b5cffee37f7bcd70aabf9a5a4de4bd4c
ProductiveRage <dangger36@gmail.com>
2016-09-13 10:36:49 +0100

    Extended the Set class with Update, UpdateAll, RemoveAt and Remove methods (also pulled in Select rather than it being an extension method)
    Implementing these methods inside the Set instance allow for more of the
    node chain to be persisted in many cases

--- ProductiveRage.Immutable/Set.cs
+++ ProductiveRage.Immutable/Set.cs
@@ -148,6 +148,214 @@ namespace ProductiveRage.Immutable
 			return new Set<T>(newNode);
 		}
 
+		/// <summary>
+		/// Since the Set class uses uint for its index value, the standard LINQ indexed Select class requires a cast from int to uint - this version prevents that cast from
+		/// being necessary
+		/// </summary>
+		public IEnumerable<TResult> Select<TResult>(Func<T, uint, TResult> selector)
+		{
+			if (selector == null)
+				throw new ArgumentNullException("selector");
+
+			uint index = 0;
+			var node = _headIfAny;
+			while (node != null)
+			{
+				yield return selector(node.Item, index);
+				node = node.NextIfAny;
+				index++;
+			}
+		}
+
+		/// <summary>
+		/// This will return a new Set of the same element type, where each item has been processed with the specified updater delegate. It is not valid for the updater to
+		/// return a null reference, this data type will not store null references (if there may be missing values then the type parameter should be an Optional). If the
+		/// set is empty or if the updater returns the same reference for every item then no change is required and the current Set reference will be returned unaltered.
+		/// </summary>
+		public Set<T> UpdateAll(Func<T, T> updater)
+		{
+			if (updater == null)
+				throw new ArgumentNullException("updater");
+
+			return UpdateInternal(updater, optionalFilter: null);
+		}
+
+		/// <summary>
+		/// This will return a new Set of the same element type, where any item that matches the specified filter will be processed with the specified updater delegate. It
+		/// is not valid for the updater to return a null reference, this data type will not store null references (if there may be missing values then the type parameter
+		/// should be an Optional). If the set is empty, if the filter does not match any items or if the updater returns the same reference for every matched item then
+		/// no change is required and the current Set reference will be returned unaltered.
+		/// </summary>
+		public Set<T> Update(Func<T, T> updater, Func<T, bool> filter)
+		{
+			if (updater == null)
+				throw new ArgumentNullException("updater");
+			if (filter == null)
+				throw new ArgumentNullException("filter");
+
+			return UpdateInternal(updater, filter);
+		}
+
+		private Set<T> UpdateInternal(Func<T, T> updater, Func<T, bool> optionalFilter)
+		{
+			if (updater == null)
+				throw new ArgumentNullException("updater");
+
+			if (Count == 0)
+				return this;
+
+			// Walk down the list, generating the updated values on the way
+			var newValues = new T[Count];
+			var node = _headIfAny;
+			Tuple<Node, int> earliestUnchangedValueAndIndex = null;
+			for (var i = 0; i < Count; i++)
+			{
+				var currentValue = node.Item;
+				var needToApplyUpdateToThisValue = (optionalFilter == null) || optionalFilter(currentValue);
+				if (!needToApplyUpdateToThisValue)
+				{
+					if (earliestUnchangedValueAndIndex == null)
+						earliestUnchangedValueAndIndex = Tuple.Create(node, i);
+					newValues[i] = currentValue;
+				}
+				else
+				{
+					var newValue = updater(currentValue);
+					if (newValue == null)
+						throw new ArgumentException("updated returned a null reference - this is not acceptable, Set<T> will not record nulls");
+					var isNewValueTheSameAsCurrentValue = newValue.Equals(currentValue);
+					if ((earliestUnchangedValueAndIndex == null) && isNewValueTheSameAsCurrentValue)
+						earliestUnchangedValueAndIndex = Tuple.Create(node, i);
+					else if (!isNewValueTheSameAsCurrentValue)
+						earliestUnchangedValueAndIndex = null;
+					newValues[i] = newValue;
+				}
+				node = node.NextIfAny;
+			}
+
+			// If we are able to persist some of the nodes, then start at that point - otherwise, we'll have to rebuild the entire list
+			int startIndexOfReusableContent;
+			if (earliestUnchangedValueAndIndex == null)
+				startIndexOfReusableContent = (int)Count;
+			else
+			{
+				if (earliestUnchangedValueAndIndex.Item2 == 0)
+				{
+					// If we're able to share the first item then we're able to share the entire list and there's no need to create a new one
+					return this;
+				}
+				startIndexOfReusableContent = earliestUnchangedValueAndIndex.Item2;
+				node = earliestUnchangedValueAndIndex.Item1;
+			}
+
+			// Now create a new list with the new values, starting before the content that may be reused (if any)
+			for (var i = startIndexOfReusableContent - 1; i >= 0; i--)
+			{
+				node = new Node
+				{
+					Count = (node == null) ? 1 : (node.Count + 1),
+					Item = newValues[i],
+					NextIfAny = node
+				};
+			}
+			return new Set<T>(node);
+		}
+
+		/// <summary>
+		/// This will throw an exception for an invalid index value or for a null value reference
+		/// </summary>
+		public Set<T> RemoveAt(uint index)
+		{
+			if (index >= Count)
+				throw new ArgumentOutOfRangeException("index");
+
+			// Walk down the list until the node-to-be-removed is reached, tracking the values passed through on the way
+			var valuesBeforeRemove = new T[index];
+			var node = _headIfAny;
+			for (var i = 0; i < index; i++)
+			{
+				valuesBeforeRemove[i] = node.Item;
+				node = node.NextIfAny;
+			}
+
+			// Move to the node (if there is one) after the one that was removed
+			node = node.NextIfAny;
+
+			// Rebuilding the list from list from here, the node chain after the removal does not need to be altered
+			for (var i = valuesBeforeRemove.Length - 1; i >= 0; i--)
+			{
+				node = new Node
+				{
+					Count = (node == null) ? 1 : (node.Count + 1),
+					Item = valuesBeforeRemove[i],
+					NextIfAny = node
+				};
+			}
+			return new Set<T>(node);
+		}
+
+		/// <summary>
+		/// This will remove any items from the set that match the specified filter. If no items were matched then the initial set will be returned unaltered.
+		/// </summary>
+		public Set<T> Remove(Func<T, bool> filter)
+		{
+			if (filter == null)
+				throw new ArgumentNullException("filter");
+
+			if (Count == 0)
+				return this;
+
+			// Walk down the list, generating the updated values on the way
+			var newValues = new Optional<T>[Count];
+			var node = _headIfAny;
+			Tuple<Node, int> earliestUnchangedValueAndIndex = null;
+			for (var i = 0; i < Count; i++)
+			{
+				if (filter(node.Item))
+				{
+					earliestUnchangedValueAndIndex = null;
+					newValues[i] = Optional<T>.Missing;
+				}
+				else
+				{
+					if (earliestUnchangedValueAndIndex == null)
+						earliestUnchangedValueAndIndex = Tuple.Create(node, i);
+					newValues[i] = node.Item;
+				}
+				node = node.NextIfAny;
+			}
+
+			// If we are able to persist some of the nodes, then start at that point - otherwise, we'll have to rebuild the entire list
+			int startIndexOfReusableContent;
+			if (earliestUnchangedValueAndIndex == null)
+				startIndexOfReusableContent = (int)Count;
+			else
+			{
+				if (earliestUnchangedValueAndIndex.Item2 == 0)
+				{
+					// If we're able to share the first item then we're able to share the entire list and there's no need to create a new one
+					return this;
+				}
+				startIndexOfReusableContent = earliestUnchangedValueAndIndex.Item2;
+				node = earliestUnchangedValueAndIndex.Item1;
+			}
+
+			// Now create a new list with the new values, starting before the content that may be reused (if any)
+			for (var i = startIndexOfReusableContent - 1; i >= 0; i--)
+			{
+				if (!newValues[i].IsDefined)
+					continue;
+
+				node = new Node
+				{
+					Count = (node == null) ? 1 : (node.Count + 1),
+					Item = newValues[i].Value,
+					NextIfAny = node
+				};
+			}
+			return new Set<T>(node);
+		}
+
 		public IEnumerator<T> GetEnumerator()
 		{
 			var node = _headIfAny;
