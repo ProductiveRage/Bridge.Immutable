90738657fb8715aa392f9eaf308df9696717e60d
ProductiveRage <dangger36@gmail.com>
2016-10-01 12:03:45 +0100

    Bumped version number - botched the last release somehow, C# 6 readonly auto-properties weren't working with IAmImmutable

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Productive Rage 2015")]
-[assembly: AssemblyVersion("1.9.5.0")]
-[assembly: AssemblyFileVersion("1.9.5.0")]
+[assembly: AssemblyVersion("1.9.6.0")]
+[assembly: AssemblyFileVersion("1.9.6.0")]
