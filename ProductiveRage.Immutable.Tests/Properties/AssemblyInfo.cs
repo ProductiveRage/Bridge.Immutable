565cae55de3f704b06f59616844118e1efc421ad
ProductiveRage <dangger36@gmail.com>
2017-04-23 16:51:18 +0100

    Will need to make changes for Bridge 16.0 so it's (past) time to introduce some client-side unit tests (previously only had Analyser tests)

---
+++ ProductiveRage.Immutable.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProductiveRage.Immutable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductiveRage.Immutable")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b1e0c64-1d2b-4d41-96d7-f9fd6c4248aa")]
---
+++ ProductiveRage.Immutable.Tests/Tests.cs
@@ -0,0 +1,84 @@
+﻿using Bridge.Html5;
+using Bridge.QUnit;
+
+namespace ProductiveRage.Immutable.Tests
+{
+	public static class Tests
+	{
+		[Ready]
+		public static void Go()
+		{
+			QUnit.Test("Simple string property CtorSet initialisation", assert =>
+			{
+				var x = new SomethingWithStringId("abc");
+				assert.Equal(x.Id, "abc");
+			});
+
+			QUnit.Test("Simple string property update using With directly", assert =>
+			{
+				var x = new SomethingWithStringId("abc");
+				x = x.With(_ => _.Id, "def");
+				assert.Equal(x.Id, "def");
+			});
+
+			QUnit.Test("Simple string property update using With indirectly", assert =>
+			{
+				var x = new SomethingWithStringId("abc");
+				var idUpdater = x.With(_ => _.Id);
+				x = idUpdater("def");
+				assert.Equal(x.Id, "def");
+			});
+
+			QUnit.Test("Simple string property update using GetProperty and With", assert =>
+			{
+				var x = new SomethingWithStringId("abc");
+				var propertyToUpdate = x.GetProperty(_ => _.Id);
+				x = x.With(propertyToUpdate, "def");
+				assert.Equal(x.Id, "def");
+			});
+
+			QUnit.Test("Single-element NonNullList<string> property update using With directly", assert =>
+			{
+				var x = new SomethingWithNonNullListStringValues(NonNullList.Of("abc", "def"));
+				x = x.With(_ => _.Values, 1, "xyz");
+				assert.Equal(x.Values[1], "xyz");
+			});
+
+			QUnit.Test("Single-element NonNullList<string> property update using GetProperty and With", assert =>
+			{
+				var x = new SomethingWithNonNullListStringValues(NonNullList.Of("abc", "def"));
+				var propertyToUpdate = x.GetProperty(_ => _.Values);
+				x = x.With<SomethingWithNonNullListStringValues, string>(propertyToUpdate, 1, "xyz");
+				assert.Equal(x.Values[1], "xyz");
+			});
+
+			QUnit.Test("Single-element Set<string> (legacy compatibility alias for NonNullList) property update using GetProperty and With", assert =>
+			{
+#pragma warning disable CS0618 // Ignore the fact that Set is obsolete
+				var x = new SomethingWithNonNullListStringValues(Set.Of("abc", "def"));
+#pragma warning restore CS0618
+				var propertyToUpdate = x.GetProperty(_ => _.Values);
+				x = x.With<SomethingWithNonNullListStringValues, string>(propertyToUpdate, 1, "xyz");
+				assert.Equal(x.Values[1], "xyz");
+			});
+		}
+
+		public sealed class SomethingWithStringId : IAmImmutable
+		{
+			public SomethingWithStringId(string id)
+			{
+				this.CtorSet(_ => _.Id, id);
+			}
+			public string Id { get; }
+		}
+
+		public sealed class SomethingWithNonNullListStringValues : IAmImmutable
+		{
+			public SomethingWithNonNullListStringValues(NonNullList<string> values)
+			{
+				this.CtorSet(_ => _.Values, values);
+			}
+			public NonNullList<string> Values { get; }
+		}
+	}
+}
