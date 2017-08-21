4be7b47650d6a0ebd03a45f69ff1419cb9de82dd
ProductiveRage <dangger36@gmail.com>
2017-08-21 21:01:48 +0100

    (2.7.0) Bump SharedAssembly.cs version as well (tried to do in last changeset but GitHub for Windows cried)

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Productive Rage 2017")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
