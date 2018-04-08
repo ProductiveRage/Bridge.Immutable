5f607df1de96b1743780a89d97191514339a8b4d
ProductiveRage <dangger36@gmail.com>
2018-04-08 16:58:33 +0100

    (2.11.0) Version bump for release

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Productive Rage 2018")]
-[assembly: AssemblyVersion("2.10.4.0")]
-[assembly: AssemblyFileVersion("2.10.4.0")]
+[assembly: AssemblyVersion("2.11.0.0")]
+[assembly: AssemblyFileVersion("2.11.0.0")]
